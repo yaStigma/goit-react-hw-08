28a4362e9012faa9bca7122f6fb970d950238ede
Tanya Yenakieva <yastigma@gmail.com>
2024-10-05 14:21:01 +0300

    fin

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,5 @@
 import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
-import HomePage from '../../page/HomePage/HomePage';
-import  ContactsPage from '../../page/ContactsPage/ContactsPage';
-import LoginPage from '../../page/LoginPage/LoginPage';
-import RegistrationPage from '../../page/RegistrationPage/RegistrationPage'
+import { useEffect, lazy, Suspense } from "react";
 // import css from "./App.module.css"
 import Layout from "../Layout/Layout";
 import { Routes, Route } from "react-router-dom";
@@ -11,6 +7,12 @@ import {  selectIsRefreshing } from "../../redux/auth/selectors";
 import { refreshUser } from "../../redux/auth/operations";
 import RestrictedRoute from "./RestrictedRoute";
 import PrivateRoute from "./PrivateRoute";
+
+
+const HomePage = lazy(() => import('../../page/HomePage/HomePage'));
+const ContactsPage = lazy (() => import ('../../page/ContactsPage/ContactsPage')) ;
+const LoginPage = lazy (()=> import('../../page/LoginPage/LoginPage'));
+const RegistrationPage = lazy (() => import('../../page/RegistrationPage/RegistrationPage'));
 
 
 export default function App() {
@@ -26,12 +28,14 @@ useEffect (() => {
     isRefreshing ? (<b>Please wait, updating user info...</b>) :
     (
     <Layout>
+      <Suspense fallback={null}>
 <Routes>
 <Route path="/" element={<HomePage />} />
 <Route path="/register" element={<RestrictedRoute component={<RegistrationPage />} redirectTo="/" />} />   
 <Route path="/login" element={ <RestrictedRoute component={<LoginPage />} redirectTo="/contacts" />} />
 <Route path="/contacts" element={ <PrivateRoute component={<ContactsPage/>} redirectTo="/login" /> } />   
 </Routes>
+</Suspense>
 </Layout>
 )
   );
